85f48847974b1b1e6673e2255e7147f42025b5d1
jcaole <jeremy_1072@yahoo.com>
2023-04-22 00:08:35 -0500

    saving progress

--- caolej/db.php
+++ caolej/db.php
@@ -1,110 +1,145 @@
 <!-------------------------------------------------------------------
-   db.php
-   This page: 
-     - demonstrates adding data to a table and issuing a query
+   file name:   db.php
+   Course:      COP4020
+   Project:     5
+   Author:      Jeremy Caole
+   Description: This file defines functions for connecting to a MySQL database 
+   and manipulating data in the project5Table.
+
  -------------------------------------------------------------------->
-<?php
-if (!isset($_SESSION)) {
-   session_start();
-}
-?>
-<html>
-  <head>
-	 <title>Database Operations</title>
-    <style>
-      body {
-        background-color: rgb(220, 220, 220);
-        background-size: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <?php
+ <?php
+// if (!isset($_SESSION)) {
+//   session_start();
+// }
 
-/*********************************************************************
-   connectToDB($wdb_host,$wdb_user,$wdb_pass,$wdb_name): mysqli
- *********************************************************************/
-if (!function_exists('connectToDB'))   {
-  function connectToDB($wdb_host,$wdb_user,$wdb_pass,$wdb_name): mysqli
-    {
-       $mysqli = new mysqli($wdb_host,$wdb_user,$wdb_pass,$wdb_name);
-       if ($mysqli -> connect_errno) {
-          echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-          exit();
-        }
-       return $mysqli; 
+/**
+ * Reads authentication values for the MySQL database from a vals file.
+ */
+if (!function_exists('read_auth_values')) {
+  function read_auth_values()
+  {
+    // Read database authentication values from vals
+    $vals_path = __DIR__ . "/auth/vals";
+    $vals = file($vals_path);
+
+    // Separate the connection parameters
+    $host = ($vals[0]);
+    $host_parts = explode(',', $host);
+    if (count($host_parts) < 4) {
+      die("Error: Invalid authentication values in $vals_path");
     }
- }
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); 
- $myfile = fopen('./auth/vals', "r") or die("Unable to open database settings file."); 
- $theParams=fread($myfile, filesize('./auth/vals'));
- fclose($myfile);
- $dbparams=explode(',',$theParams);
+    $db_host = $host_parts[0];
+    $db_user = $host_parts[1];
+    $db_pass = $host_parts[2];
+    $db_name = $host_parts[3];
 
- try
- {
-   $mysqli = connectToDB($dbparams[0],$dbparams[1],$dbparams[2],$dbparams[3]); 
-   $query = "select * from demotable";
-   print "query".$query."<br>";
-   $stmt = $mysqli->prepare($query);
-   $stmt->execute();
-   if ($result = $stmt->get_result()) 
-   {
-     while ($row = $result->fetch_array(MYSQLI_NUM)) 
-     { 
-        for($i=0; $i<3;$i++)
-          print $row[$i]."<br>"; 
-     }
-     $result->free(); 
-     $stmt->close();        
-   }
- } 
- catch (mysqli_sql_exception $e) 
- { 
-   echo "MySQLi Error Code: " . $e->getCode() . "<br />";
-   echo "Exception Msg: " . $e->getMessage();
-   exit; // exit and close connection.
- }
- $mysqli -> close();
- unset($_POST['hiddenField']);
+    return [$db_host, $db_user, $db_pass, $db_name];
+  }
+}
 
-/*
-create database testDB;
+/**
+ * Creates a new MySQLi connection using the parameters.
+ */
+if (!function_exists('connect_to_database')) {
+  function connect_to_database($db_host, $db_user, $db_pass, $db_name)
+  {
+    // Create a new MySQLi connection
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-CREATE TABLE `testTable` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=CSV DEFAULT CHARSET=utf8mb4;
+    // Check if the connection was successful
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+  }
+}
 
-INSERT INTO testtable VALUES (1,'name1','description1');
-INSERT INTO testtable VALUES (2,'name2','description2');
-INSERT INTO testtable VALUES (3,'name3','description3');
-INSERT INTO testtable VALUES (4,'name4','description4');
-INSERT INTO testtable VALUES (5,'name5','description5');
+/**
+ * Drops the project5Table from the connected database if it exists.
+ *
+ * @param mysqli $conn The MySQLi connection.
+ */
+if (!function_exists('drop_table')) {
+  function drop_table($conn)
+  {
+    // Drop table if exists
+    $sql = "DROP TABLE IF EXISTS project5Table";
+    if ($conn->query($sql) === TRUE) {
+      print "command [drop TABLE project5Table]<br>";
+    } else {
+      print "Error dropping table: " . $conn->error;
+    }
+  }
+}
 
-select * from testTable;
-*/
+/**
+ * Creates a MySQL table based on given attributes
+ */
+if (!function_exists('create_table')) {
+  function create_table($conn)
+  {
+    // Create table
+    $sql = "CREATE TABLE project5Table (
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    street_address varchar(50) NOT NULL,
+    city varchar(20) NOT NULL,
+    state_name varchar(5) NOT NULL,
+    zipcode text NOT NULL,
+    age varchar(5) NOT NULL,
+    salary varchar(15) NOT NULL
+  ) ENGINE=CSV DEFAULT CHARSET=utf8mb4";
+
+    if ($conn->query($sql) === TRUE) {
+      print "Table project5Table created successfully";
+    } else {
+      print "Error creating table: " . $conn->error;
+    }
+    print "command [$sql]<br><br>";
+  }
+}
 
 
-/*
-create database project5DB;
+/**
+ * Reads data from a CSV file and inserts it into a MySQL table
+ */
+if (!function_exists('read_file')) {
+  function read_file($conn)
+  {
+    $filename = "data.csv";
+    $file = fopen($filename, "r");
 
-CREATE TABLE `project5Table` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=CSV DEFAULT CHARSET=utf8mb4;
+    while (($line = fgets($file)) !== false) {
+      $data = explode(";", ($line));
 
-INSERT INTO project5Table VALUES (1,'name1','description1');
-INSERT INTO project5Table VALUES (2,'name2','description2');
-INSERT INTO project5Table VALUES (3,'name3','description3');
-INSERT INTO project5Table VALUES (4,'name4','description4');
-INSERT INTO project5Table VALUES (5,'name5','description5');
+      print implode(";<br>", $data) . "<br><br>";
 
-select * from project5Table;
-*/
+      foreach ($data as $entry) {
+        $fields = explode(",", $entry);
+
+        $first_name = $fields[0];
+        $last_name = $fields[1];
+        $street_address = $fields[2];
+        $city = $fields[3];
+        $state_name = $fields[4];
+        $zipcode = $fields[5];
+        $age = $fields[6];
+        $salary = $fields[7];
+
+        $sql = "INSERT INTO project5Table (first_name, last_name, street_address, city, state_name, zipcode, age, salary)
+          VALUES ('$first_name', '$last_name', '$street_address', '$city', '$state_name', '$zipcode', '$age', '$salary')";
 
 
+        if ($conn->query($sql) === TRUE) {
+          print "command insert into project5Table values(\"$first_name\",\"$last_name\",\"$street_address\",\"$city\",\"$state_name\",\"$zipcode\",\"$age\",\"$salary\");<br>";
+        } else {
+          print "Error inserting data: " . $conn->error;
+        }
+      }
+    }
+    fclose($file);
+    $conn->close();
+  }
+}
 
 ?>
